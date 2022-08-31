32e991bc1f409ba6b6e1e082ced2adc8f7f3ffda
siropaca <ryhshr3@gmail.com>
2022-08-31 20:57:20 +0900

    :sparkles: タイトルとlang属性を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>ここにタイトル</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1,initial-scale=1,width=device-width'
+        />
+        <meta
+          content='ここに説明'
+          name='description'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
