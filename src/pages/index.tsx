fdb5dd9b234b680964034964fce9fa20ffba9139
siropaca <ryhshr3@gmail.com>
2022-08-31 20:36:04 +0900

    :construction: DBからデータ取得まで

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,35 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import { MainLayout } from '@/components/Layout';
+import { supabase } from '@/lib/supabaseClient';
+import { useState, useEffect } from 'react';
 
 const Home: NextPage = () => {
+  const [bucketItems, setBucketItems] = useState<any[] | null>();
+  const [errorMessage, setErrorMessage] = useState<string | null>();
+
+  useEffect(() => {
+    (async () => {
+      const { data: bucket_items, error } = await supabase.from('bucket_items').select('*');
+      setBucketItems(bucket_items);
+      setErrorMessage(error?.message);
+    })();
+  }, []);
+
+  if (errorMessage) {
+    return <MainLayout>{errorMessage}</MainLayout>;
+  }
+
+  if (!bucketItems) {
+    return <MainLayout>取得中...</MainLayout>;
+  }
+
   return (
     <MainLayout>
       <div>トップページ</div>
+      {bucketItems.map((item) => (
+        <p key={item['id']}>{item['label']}</p>
+      ))}
     </MainLayout>
   );
 };
