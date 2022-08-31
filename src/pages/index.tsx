a4e9afea8a83423db3a73042936c0537099a9fca
siropaca <ryhshr3@gmail.com>
2022-08-31 20:44:28 +0900

    :construction: 不要なimportを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 import { MainLayout } from '@/components/Layout';
 import { supabase } from '@/lib/supabaseClient';
-import { useState, useEffect } from 'react';
+import type { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 
 const Home: NextPage = () => {
   const [bucketItems, setBucketItems] = useState<any[] | null>();
